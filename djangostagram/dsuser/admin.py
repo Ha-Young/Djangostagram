8b9e739238b966d4dca083c37d4f60cf6ec66a83
hychoi <hayeong28@naver.com>
2020-05-16 18:20:56 +0900

    make timeline index page && make base.html with top-nav

--- djangostagram/dsuser/admin.py
+++ djangostagram/dsuser/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Dsuser
 
 # Register your models here.
+
+
+class DsuserAdmin(admin.ModelAdmin):
+    list_display=('user_id', 'email', 'registered_dttm')
+
+admin.site.register(Dsuser, DsuserAdmin)
