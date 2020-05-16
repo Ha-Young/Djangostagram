fa3878eb3aa9751e1d9fb104b12020adaf1bf40f
hychoi <hayeong28@naver.com>
2020-05-16 18:59:29 +0900

    make login form

--- djangostagram/dsuser/forms.py
+++ djangostagram/dsuser/forms.py
@@ -39,7 +39,6 @@ class RegisterForm(forms.Form):
     )
 
     def clean(self):
-        print("clean")
         cleaned_data = super().clean()
         
         user_id = cleaned_data.get('user_id')
@@ -56,5 +55,51 @@ class RegisterForm(forms.Form):
                 self.add_error('re_password', '비밀번호가 서로 다릅니다')
                 self.error = "비밀번호가 서로 다릅니다"
                 return
+        else:
+            self.error = "모든 값을 입력해야합니다"
+
+class LoginForm(forms.Form):
+    user_id = forms.CharField(
+        label="아이디",
+        required=True, 
+        error_messages={
+            'required': '아이디를 입력하십시오'
+        }
+    )
+
+    password = forms.CharField(
+        label="비밀번호",
+        required=True, 
+        error_messages={
+            'required': '비밀번호를 입력하십시오'
+        },
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        
+        user_id = cleaned_data.get('user_id')
+        password = cleaned_data.get('password')
+
+        if user_id and password:
+            try:
+                login_dsuser = Dsuser.objects.get(user_id=user_id)
+            except Dsuser.DoseNotExist as e:
+                print(e)
+                self.add_error('user_id', '아이디가 없습니다')
+                self.error = "아이디가 없습니다"
+                return
+            except Exception as e:
+                print(e)
+                self.add_error('user_id', '알수없는 오류')
+                self.error("알수없는 오류 발생")
+                return
+
+            if not check_password(password, login_dsuser.password):
+                self.add_error('password', '비밀번호를 틀렸습니다')
+                self.error("비밀번호를 틀렸습니다")
+            else:
+                self.pk = login_dsuser.pk
         else:
             self.error = "모든 값을 입력해야합니다"
