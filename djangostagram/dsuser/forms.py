24a75df2891f3259f00cd44061aaa639da50a9c6
hychoi <hayeong28@naver.com>
2020-05-16 18:50:39 +0900

    complete register

---
+++ djangostagram/dsuser/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from .models import Dsuser
+from django.contrib.auth.hashers import check_password, make_password
+
+
+class RegisterForm(forms.Form):
+    user_id = forms.CharField(
+        label="아이디",
+        required=True, 
+        error_messages={
+            'required': '아이디를 입력하십시오'
+        }
+    )
+
+    email = forms.EmailField(
+        label="이메일",
+        required=True, 
+        error_messages={
+            'required': '이메일을 입력하십시오'
+        }
+    )
+
+    password = forms.CharField(
+        label="비밀번호",
+        required=True, 
+        error_messages={
+            'required': '비밀번호를 입력하십시오'
+        },
+        widget=forms.PasswordInput
+    )
+
+    re_password = forms.CharField(
+        label="비밀번호 확인",
+        required=True, 
+        error_messages={
+            'required': '비밀번호를 입력하십시오'
+        },
+        widget=forms.PasswordInput
+    )
+
+    def clean(self):
+        print("clean")
+        cleaned_data = super().clean()
+        
+        user_id = cleaned_data.get('user_id')
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        re_password = cleaned_data.get('re_password')
+
+        print(password, re_password)
+
+        if user_id and email and password and re_password:
+            if password != re_password:
+                print("password not same")
+                self.add_error('password', '비밀번호가 서로 다릅니다')
+                self.add_error('re_password', '비밀번호가 서로 다릅니다')
+                self.error = "비밀번호가 서로 다릅니다"
+                return
+        else:
+            self.error = "모든 값을 입력해야합니다"
--- djangostagram/dsuser/views.py
+++ djangostagram/dsuser/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.contrib.auth.hashers import check_password, make_password
+from .models import Dsuser
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def timeline(request):
     return render(request, 'timeline.html')
+
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        new_dsuser = Dsuser(
+            user_id=form.data.get('user_id'),
+            email=form.data.get('email'),
+            password=make_password(form.data.get('password'))
+        )
+        new_dsuser.save()
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):        
+        return super().form_invalid(form)
