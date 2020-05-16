e47e789701f2fd6456f4ff5b545ac335b8013498
hychoi <hayeong28@naver.com>
2020-05-16 21:25:36 +0900

    make post model

---
+++ djangostagram/post/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    writer = models.ForeignKey("dsuser.Dsuser", verbose_name="작성자", on_delete=models.CASCADE)
+    img_url = models.CharField(verbose_name='이미지 주소', max_length=512)
+    description = models.TextField(verbose_name='내용')
+    tags = models.ManyToManyField("tag.Tag", verbose_name="태그")
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name="작성일")
+
+    def __str__(self):
+        return self.user_id
+
+    class Meta:
+        db_table = '글'
+        managed = True
+        verbose_name = '글'
+        verbose_name_plural = '글s'
